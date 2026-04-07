e711a9e88768885f1c4001eb31047e8be32eb266
ApostolosG <apostolosg@mentalmedia.gr>
2026-04-07 10:49:51 +0300

    feat(shipping): update catalog description for same day delivery sample

--- src/apog_shipping_same_day/upload/catalog/language/el-gr/extension/shipping/apog_same_day.php
+++ src/apog_shipping_same_day/upload/catalog/language/el-gr/extension/shipping/apog_same_day.php
@@ -5,10 +5,18 @@ $_['text_description'] = 'Αυθημερόν Παράδοση Θεσσαλονί
 
 // HTML info for selected shipping
 $_['text_same_day_info'] = '
-    <div class="same-day-info" style="margin: 8px 0 8px 6px;p;padding: 10px 12px 10px 20px;ackground:#f9f9f9;border-left:3px solid #428bca;font-size:12px;color:#555;line-height:1.5;max-width:320px;">
-        <span>Για παραγγελίες Δευτέρα–Παρασκευή έως τις 13:00</span>
-        <br><br>
-        <strong>Ισχύει για τις περιοχές:</strong><br>
-        <span>Αμπελόκηποι, Μενεμένη, Θεσσαλονίκη, Τριανδρία, Καλαμαριά, Ελευθέριο-Κορδελιό, Εύοσμος, Άγιος Παύλος, Νεάπολη, Πεύκα, Συκιές, Ευκαρπία, Πολίχνη, Σταυρούπολη, Πανόραμα, Πυλαία, Θέρμη, Μίκρα</span>
+    <div class="same-day-info" style="margin: 8px 0px 12px 28px;padding: 10px 12px;background: rgb(249, 249, 249);border-left: 3px solid rgb(66, 139, 202);font-size: 12px;color: rgb(85, 85, 85);line-height: 1.5;max-width: 320px;display: flex;flex-direction: column;gap: 1rem;">
+        <span class="sdi-note" style="display: flex;flex-direction: column;gap: 0.5rem;">
+            <strong>Για παραγγελίες Δευτέρα–Παρασκευή έως τις 13:00</strong>
+            <span class="sdi-delivery-hours" style="display: flex;flex-direction: column;gap: 0.25rem;justify-content: center;">
+                <span>* Ώρες παράδοσης (13.00 - 17.00)</span>
+                <span>* Οι παραγγελίες μετά τις 13:00 παραδίδονται την επόμενη εργάσιμη ημέρα.</span>
+            </span>
+        </span>
+        
+        <span class="sdi-note-areas" style="display: flex;flex-direction: column;gap: 0.5rem;">
+            <strong class="sdi-areas-title">Ισχύει για τις περιοχές:</strong>
+            <span class="sdi-areas">Αμπελόκηποι, Μενεμένη, Θεσσαλονίκη, Τριανδρία, Καλαμαριά, Ελευθέριο-Κορδελιό, Εύοσμος, Άγιος Παύλος, Νεάπολη, Πεύκα, Συκιές, Ευκαρπία, Πολίχνη, Σταυρούπολη, Πανόραμα, Πυλαία, Θέρμη, Μίκρα</span>
+        </span>
     </div>
 ';
--- src/apog_shipping_same_day/upload/catalog/language/en-gb/extension/shipping/apog_same_day.php
+++ src/apog_shipping_same_day/upload/catalog/language/en-gb/extension/shipping/apog_same_day.php
@@ -5,10 +5,18 @@ $_['text_description'] = 'Same Day Delivery Thessaloniki';
 
 // HTML info for selected shipping
 $_['text_same_day_info'] = '
-    <div class="same-day-info" style="margin: 8px 0 8px 6px; padding: 10px 12px 10px 20px; background:#f9f9f9; border-left:3px solid #428bca; font-size:12px; color:#555; line-height:1.5; max-width:320px;">
-        <span>For orders Monday–Friday until 13:00</span>
-        <br><br>
-        <strong>Applies to the areas:</strong><br>
-        <span>Ampelokipoi, Menemeni, Thessaloniki, Triandria, Kalamaria, Eleftherio-Kordelio, Evosmos, Agios Pavlos, Neapoli, Pefka, Sykees, Efkarpia, Polichni, Stavroupoli, Panorama, Pylaia, Thermi, Mikra</span>
+    <div class="same-day-info" style="margin: 8px 0px 12px 28px;padding: 10px 12px;background: rgb(249, 249, 249);border-left: 3px solid rgb(66, 139, 202);font-size: 12px;color: rgb(85, 85, 85);line-height: 1.5;max-width: 320px;display: flex;flex-direction: column;gap: 1rem;">
+        <span class="sdi-note" style="display: flex;flex-direction: column;gap: 0.5rem;">
+            <strong>For orders Monday–Friday until 13:00</strong>
+            <span class="sdi-delivery-hours" style="display: flex;flex-direction: column;gap: 0.25rem;justify-content: center;">
+                <span>* Delivery hours (13.00 - 17.00)</span>
+                <span>* Orders placed after 13:00 are delivered the next working day.</span>
+            </span>
+        </span>
+        
+        <span class="sdi-note-areas" style="display: flex;flex-direction: column;gap: 0.5rem;">
+            <strong class="sdi-areas-title">Applies to the areas:</strong>
+            <span class="sdi-areas">Ampelokipoi, Menemeni, Thessaloniki, Triandria, Kalamaria, Eleftherio-Kordelio, Evosmos, Agios Pavlos, Neapoli, Pefka, Sykees, Efkarpia, Polichni, Stavroupoli, Panorama, Pylaia, Thermi, Mikra</span>
+        </span>
     </div>
 ';
